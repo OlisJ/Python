4e483b05ac18340e665dcc35369e192ecd49e183
OlisJ <olisj794@gmail.com>
2025-06-12 17:17:11 +0200

    module 10 challenge solution

--- module 10/challenge.py
+++ module 10/challenge.py
@@ -37,11 +37,52 @@ class DigitalSchool:
     def courses(self,courses):
         self.__courses=courses
 
-show_school_info=DigitalSchool("DigitalSchool","Prishtina","Kosova","Python")
-print(show_school_info)
+#defining methods
+
+    def show_school_info(self):
+        return {
+            "name":self.__name,
+            "city":self.__city,
+            "state":self.__state,
+            "courses":self.__courses
+        }
 
 
+
+    def organize_hackathon(self):
+        print("Organizing a generic hackathon.")
+
+#define a subclass called Ds Prishtina where it has a private attr called student number and 2 methods
 class DS_Prishtina(DigitalSchool):
-    def __init__(self,student_number):
+    def __init__(self,name,city,state,courses,student_number):
+        super().__init__(name,city,state,courses)
         self.__student_number=student_number
 
+    def SCF(self):
+        print("First edition")
+
+    def organize_hackathon(self):
+        print("Were doing a data analysis hackathon")
+
+#Implementing
+
+prishtina_obj=DS_Prishtina("Ds-Prishtina","Prishtina","Kosova",["python",'html','css','java'],300)
+prishtina_obj.organize_hackathon()
+
+#define the subclass dsferizaj inhereting from digitalschool
+
+class DS_Ferizaj(DigitalSchool):
+    def __init__(self,name,city,state,courses,student_number):
+        super().__init__(name,city,state,courses)
+        self.__student_number=student_number
+
+    def organize_hackathon(self):
+        print("Organizing the first dsFerizaj hackathon")
+
+    def internship(self):
+        print("We are looking for applicants for a DigitalSchool Internship")
+
+ferizaj=DS_Ferizaj("ds-ferizaj",'ferizaj','Kosova',['html','css','javascrip'],244)
+print(ferizaj.internship())
+print(ferizaj.organize_hackathon())
+print(ferizaj.show_school_info())
