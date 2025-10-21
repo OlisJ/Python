d53479a6845accd5bf7fa362f2efd7585a97e2f5
OlisJ <olisj794@gmail.com>
2025-10-21 17:27:26 +0200

    .

--- modul24/main.py
+++ modul24/main.py
@@ -6,7 +6,7 @@ cursor=connention.cursor()
 
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS employess(
-        id INTEGER PRIMARY KEY,
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT NOT NULL,
         position TEXT NOT NULL,
         department TEXT NOT NULL,
@@ -15,15 +15,21 @@ cursor.execute('''
 ''')
 
 
+
 connention.commit()
 
 
 cursor.execute('''
     INSERT INTO employess(name,position,department,salary)
     VALUES (?,?,?,?)'''
-               ,('John Doe','SWD' , 'IT', 700,00))
+               ,('John Doe','SWD' , 'IT', 70000))
                
                
 connention.commit()
 
 cursor.execute("SELECT * FROM employess")
+
+
+rows=cursor.fetchall()
+for row in rows:
+    print(f"employess : {rows[0]},workPlace: {row[1]}")
