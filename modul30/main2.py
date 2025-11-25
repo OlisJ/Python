6aae647dc283c57aa6a14b03dd1e4b61bf1816d2
OlisJ <olisj794@gmail.com>
2025-11-25 17:30:02 +0100

    .

--- modul30/main2.py
+++ modul30/main2.py
@@ -9,3 +9,7 @@ except requests.exceptions.HTTPError as http_err:
     print(f"HTTP error occurred: {http_err}")
 except requests.exceptions.ConnectionError as con_err:
     print(f"Connection error occurred: {con_err}")
+except requests.exceptions.Timeout as timeout_err:
+    print(f"Timeout error occurred:{timeout_err}")
+except requests.exceptions.RequestException as req_err:
+    print(f"An error occurred {req_err}")
